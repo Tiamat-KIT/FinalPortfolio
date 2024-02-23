356a52678ec787d84111e0f7b3498482d9d2f895
Amo_Taiki_from_Desktop <c1101081@planet.kanazawa-it.ac.jp>
2024-02-24 01:16:13 +0900

    Update return type of Page function

--- src/app/blog/[_id]/page.tsx
+++ src/app/blog/[_id]/page.tsx
@@ -2,7 +2,7 @@ import { getArticlesById } from "@/lib/newt"
 import parse from 'html-react-parser'
 import { ModalProps } from "@/app/@modal/(.)blog/[_id]/page"
 
-export default async function Page(params: ModalProps) {
+export default async function Page(params: ModalProps):Promise<JSX.Element> {
     const article = await getArticlesById(params._id)
     return (
         <>
