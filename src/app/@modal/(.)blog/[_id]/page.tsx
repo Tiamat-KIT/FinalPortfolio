eb75debc48b4a744b0bcb814ecc801259b7e7363
Amo_Taiki_from_Desktop <c1101081@planet.kanazawa-it.ac.jp>
2024-02-24 01:46:16 +0900

    Refactor ShowModal function to use async/await and optional chaining

--- src/app/@modal/(.)blog/[_id]/page.tsx
+++ src/app/@modal/(.)blog/[_id]/page.tsx
@@ -1,34 +1,20 @@
-"use client"
 import Modal from "@/components/parts/Modal"
-import { BlogPost, getArticlesById } from "@/lib/newt"
+import { getArticlesById } from "@/lib/newt"
 import parse from 'html-react-parser'
-import {useEffect, useState} from "react"
 
 type ModalProps =  Readonly<{
     _id: string
 }>
 
-function ShowModal(params: ModalProps): JSX.Element{
-    /* const article = await getArticlesById(params._id) */
-    const [article, setArticle] = useState<BlogPost>()
-    useEffect(() => {
-        const fetchArticle = async () => {
-            const fetchedArticle = await getArticlesById(params._id)
-            setArticle(fetchedArticle as BlogPost)
-        }
-        fetchArticle()
-    }, [params._id])
+async function ShowModal(params: ModalProps): Promise<JSX.Element>{
+    const article = await getArticlesById(params._id)
     
-        return (
-            <Modal>
-                {article && (
-                    <>
-                        <h1 className="text-3xl text-center font-bold">{article.title}</h1>
-                        <div className="container mx-auto h-auto prose">{parse(article.body as string)}</div>
-                    </>
-                )}
-            </Modal>
-       )
+    return (
+        <Modal>
+            <h1 className="text-3xl text-center font-bold">{article?.title}</h1>
+            <div className="container mx-auto h-auto prose">{parse(article?.body as string)}</div>
+        </Modal>
+   )
 }
 
 export default ShowModal
