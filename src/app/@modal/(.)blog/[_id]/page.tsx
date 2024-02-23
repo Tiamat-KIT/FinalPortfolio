a58e46153326c000be4f8746ca99456ca1e53650
Amo_Taiki_from_Desktop <c1101081@planet.kanazawa-it.ac.jp>
2024-02-24 01:39:07 +0900

    Refactor ShowModal component to use useState and useEffect hooks

--- src/app/@modal/(.)blog/[_id]/page.tsx
+++ src/app/@modal/(.)blog/[_id]/page.tsx
@@ -1,20 +1,34 @@
+"use client"
 import Modal from "@/components/parts/Modal"
-import { getArticlesById } from "@/lib/newt"
+import { BlogPost, getArticlesById } from "@/lib/newt"
 import parse from 'html-react-parser'
+import {useEffect, useState} from "react"
 
 type ModalProps =  Readonly<{
     _id: string
 }>
 
-async function ShowModal(params: ModalProps): Promise<JSX.Element>{
-    const article = await getArticlesById(params._id)
+function ShowModal(params: ModalProps): JSX.Element{
+    /* const article = await getArticlesById(params._id) */
+    const [article, setArticle] = useState<BlogPost>()
+    useEffect(() => {
+        const fetchArticle = async () => {
+            const fetchedArticle = await getArticlesById(params._id)
+            setArticle(fetchedArticle as BlogPost)
+        }
+        fetchArticle()
+    }, [params._id])
     
-    return (
-        <Modal>
-            <h1 className="text-3xl text-center font-bold">{article?.title}</h1>
-            <div className="container mx-auto h-auto prose">{parse(article?.body as string)}</div>
-        </Modal>
-   )
+        return (
+            <Modal>
+                {article && (
+                    <>
+                        <h1 className="text-3xl text-center font-bold">{article.title}</h1>
+                        <div className="container mx-auto h-auto prose">{parse(article.body as string)}</div>
+                    </>
+                )}
+            </Modal>
+       )
 }
 
 export default ShowModal
