850e8d74aff1012a676c5a3930a79b5067295b12
ｳﾀｶﾀｷｮｳｽｲ <c1101081@planet.kanazawa-it.ac.jp>
2024-02-29 09:58:40 +0900

    Update Modal.tsx

--- src/components/parts/Modal.tsx
+++ src/components/parts/Modal.tsx
@@ -8,7 +8,7 @@ export default function Modal({children}: Readonly<{children: any}>) {
    const router = useRouter()
    const onDismiss = useCallback(() => router.back(), [router])
     return (
-        <div className="bg-slate-600/30 dark:bg-slate-200/30 w-25 h-25 rounded-lg ">
+        <div className="bg-slate-600/30 dark:bg-slate-200/30 w-25 h-25 rounded-lg z-[9999]">
             <div className="mb-3">
                 <Button onClick={onDismiss}>Close</Button>
             </div>
