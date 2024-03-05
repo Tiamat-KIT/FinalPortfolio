713a5feee53d205d4716217baecf928edbeb2cf9
ｳﾀｶﾀｷｮｳｽｲ <c1101081@planet.kanazawa-it.ac.jp>
2024-03-05 22:51:33 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ export default async function Home() {
         </div>
       </div>
       <div className="info-card mt-5">
-        <iframe className="border-none m-3 rounded-xl" src="https://www.noway-form.com/ja/f/34c845b7-a334-4ec2-8df4-9f81b0f59f0d/embed" / >
+        <iframe className="border-none m-3 rounded-xl h-full" src="https://www.noway-form.com/ja/f/34c845b7-a334-4ec2-8df4-9f81b0f59f0d/embed" / >
       </div>
     </main>
   );
