f7cc60ca21c32a47906023a64c2d220c4fa63601
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2024-03-05 15:19:26 +0000

    これでいけるかな

--- src/components/parts/iframeEl.tsx
+++ src/components/parts/iframeEl.tsx
@@ -4,7 +4,8 @@ export default function IframeForm(){
   useEffect(() => {
     const iframeEl = document.querySelector("iframe") as HTMLIFrameElement
     iframeEl.addEventListener("load",  () => {
-      iframeEl.contentDocument!.body.style.margin = "3px" as string
+      const IframeInForm = iframeEl.contentDocument?.documentElement.querySelector("form") as HTMLFormElement
+      IframeInForm.style.margin = "5px"
     })
   },[])
   return <iframe className="w-full h-[1237px]" src="https://www.noway-form.com/ja/f/34c845b7-a334-4ec2-8df4-9f81b0f59f0d/embed" />
